0de791ea086ad70090294eef57fefe8f18a203d9
MarvHetz <hetzermarvin@dv-schulen.de>
2023-01-09 09:21:25 +0100

    Unterabfragen fertig

--- Lieferant.sql
+++ Lieferant.sql
@@ -19,6 +19,12 @@ insert into Lieferant values(sys_guid(),'Fischer','Fischersteig',19,34567,'Fisch
 
 update Artikel set Lieferant = (select lieferant_id from Lieferant where name = 'Fischer') where Artikelnummer in (10000,12000);
 
-update Artikel set Lieferant = (select Lieferant_id from Lieferant wherer name = 'Schlosser') where Artikelnummer between 10001 and 10103;
+update Artikel set Lieferant = (select Lieferant_id from Lieferant where name = 'Schlosser') where Artikelnummer between 10001 and 10103;
 
-update Artikel set Lieferant = (select Lieferant_id from Lieferant wherer name = 'Müller') where Artikelnummer like '11___';
+update Artikel set Lieferant = (select Lieferant_id from Lieferant where name = 'Müller') where Artikelnummer like '11___';
+
+select artikel from Artikel where lieferant in (select lieferant_id from Lieferant where name = 'Fischer' or name = 'Schlosser');
+
+select name, plz from lieferant where lieferant_id in (select lieferant from artikel where Preisgruppe between 'A' and 'D');
+
+select a.Artikelnummer, a.artikel, l.name, l.plz from artikel a, lieferant l where a.lieferant = l.lieferant_id and a.lieferant in (select lieferant_id from lieferant where name not like 'Schlosser');
